558b7b11e354e0ad173f00623dbc3d578a05d6e3
EMARTDC <122500356+EMARTDC@users.noreply.github.com>
2023-04-18 21:47:05 -0700

    added classes and added the shooting function

--- app.py
+++ app.py
@@ -1,40 +1,55 @@
 import pygame
+import sys
 pygame.init()
 
-win = pygame.display.set_mode((500,500))
+win = pygame.display.set_mode((800,600))
+clock = pygame.time.Clock
 pygame.display.set_caption("Survival game")
 
-x = 50
-y = 50
-width = 40
-height = 60
-speed = 1
+player_x = 250
+player_y = 250
+player_width = 25
+player_height = 25
 
-run = True
+class Character:
+    def _init_(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+    
+    def main(self, win):
+        pygame.draw.rect(win, (255,0,0), (self.x, self.y, self.width, self.height))
 
-while run:
-    pygame.time.delay(3)
+player = Character(player_x, player_y, player_width, player_height)
+
+
+class enemy:
+    def _init_ (self,x,y,width,height,health,attack):
+        pass
+    def main(self,dispay):
+        pass
+
+class powerups:
+    def _init_(self,x,y,width,color,height,modifier,name):
+        pass
+
+class projectile:
+    def _init_(self,x,y,width,height,color,speed):
+        pass
+
+
+while True:
+    win.fill((0,0,0)) 
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            sys.exit()
+            pygame.QUIT
 
-    keys = pygame.key.get_pressed()
-    
-    if keys[pygame.K_LEFT]:
-        x -= speed
-
-    if keys[pygame.K_RIGHT]:
-        x += speed
-
-    if keys[pygame.K_UP]:
-        y -= speed
-
-    if keys[pygame.K_DOWN]:
-        y += speed
-    
-    win.fill((0,0,0))  
-    pygame.draw.rect(win, (255,0,0), (x, y, width, height))   
+    player.main(win)
+     
+    clock.tick(60)
     pygame.display.update() 
     
 pygame.quit()
